7eb57fb829aa1fc57c5c769ffc742a46ae695109
React <3809164@gmail.com>
2023-03-27 22:42:24 +0300

    finishBrand

--- brand/src/App.js
+++ brand/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import AllInOne from "./Components/AllInOne";
+import Pricing from "./Components/Pricing";
+import Support from "./Components/Support";
+import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Hero/>
+            <About/>
+            <Support/>
+            <AllInOne/>
+            <Pricing/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
